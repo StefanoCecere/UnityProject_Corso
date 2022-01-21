9eccfcfe98f4b7764c4d00ce012026b9d6873f2e
Stefano Cecere <stefano.cecere@krur.com>
2022-01-21 12:10:26 +0100

    metodo

--- GameProject_Unity/Assets/Scripts/script.cs
+++ GameProject_Unity/Assets/Scripts/script.cs
@@ -7,12 +7,17 @@ public class script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void Ciao()
+    {
+
     }
 }
